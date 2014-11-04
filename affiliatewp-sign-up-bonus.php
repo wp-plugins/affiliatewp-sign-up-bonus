6b9e350f6dfd7089957991a61b1d0a8253317b78
sumobi <sumobi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-04 20:50:45 +0000

    v1.0
    git-svn-id: https://plugins.svn.wordpress.org/affiliatewp-sign-up-bonus/trunk@1019796 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ affiliatewp-sign-up-bonus.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * Plugin Name: AffiliateWP - Sign Up Bonus
+ * Plugin URI: http://affiliatewp.com/addons/sign-up-bonus/
+ * Description: Entice more affiliates to register by offering them a sign up bonus
+ * Author: Pippin Williamson and Andrew Munro
+ * Author URI: http://affiliatewp.com
+ * Version: 1.0
+ * Text Domain: affiliatewp-sign-up-bonus
+ * Domain Path: languages
+ *
+ * AffiliateWP is distributed under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * any later version.
+ *
+ * AffiliateWP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AffiliateWP. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package Sign Up Bonus
+ * @category Core
+ * @author Andrew Munro
+ * @version 1.0
+ */
+
+
+/**
+ * Create the signup bonus
+ * 
+ * @since 1.0
+ */
+function affwp_sub_create_bonus( $affiliate_id = 0 ) {
+
+	// return if no affiliate ID
+	if ( ! $affiliate_id ) {
+		return;
+	}
+
+	// get the sign up bonus
+	$sign_up_bonus = affiliate_wp()->settings->get( 'sign_up_bonus' ); 
+
+	// return if no sign up bonus
+	if ( ! $sign_up_bonus ) {
+		return;
+	}
+
+	$data = array(
+		'affiliate_id' => $affiliate_id,
+		'amount'       => $sign_up_bonus,
+		'description'  => __( 'Sign Up Bonus', 'affiliatewp-sign-up-bonus' ),
+		'status'       => 'unpaid'
+	);
+
+	// insert new referral for the sign up bonus
+	affwp_add_referral( $data );
+}
+
+/**
+ * Create a signup bonus when an affiliate registers
+ * 
+ * @since 1.0
+ */
+function affwp_sub_create_bonus_at_registration( $affiliate_id ) {
+
+	// return if affiliates must be approved first
+	if ( affiliate_wp()->settings->get( 'require_approval' ) ) {
+		return;
+	}
+	
+	// create the sign up bonus
+	affwp_sub_create_bonus( $affiliate_id );
+
+}
+add_action( 'affwp_insert_affiliate', 'affwp_sub_create_bonus_at_registration' );
+
+/**
+ * Create the signup bonus once the affiliate has been approved
+ * 
+ * @since 1.0
+ */
+function affwp_sub_create_bonus_after_approval( $affiliate_id, $status, $old_status ) {
+
+	if ( 'active' != $status && 'pending' == $old_status ) {
+		return;
+	}
+
+	// create the sign up bonus
+	affwp_sub_create_bonus( $affiliate_id );
+
+}
+add_action( 'affwp_set_affiliate_status', 'affwp_sub_create_bonus_after_approval', 10, 3 );
+
+/**
+ * Settings
+ * 
+ * @since 1.0
+*/
+function affwp_sub_admin_settings( $settings = array() ) {
+
+	$settings[ 'sign_up_bonus' ] = array(
+		'name' => __( 'Affiliate Sign Up Bonus', 'affiliatewp-sign-up-bonus' ),
+		'desc' => __( 'Enter the amount an affiliate should receive when they register.', 'affiliatewp-sign-up-bonus' ),
+		'type' => 'number',
+		'size' => 'small',
+		'step' => '1.0',
+		'std' => ''
+	);
+
+	return $settings;
+
+}
+add_filter( 'affwp_settings_integrations', 'affwp_sub_admin_settings' );
---
+++ readme.txt
@@ -0,0 +1,48 @@
+=== AffiliateWP - Sign Up Bonus ===
+Contributors: sumobi, mordauk
+Tags: AffiliateWP, affiliate, Pippin Williamson, Andrew Munro, mordauk, pippinsplugins, sumobi, ecommerce, e-commerce, e commerce, selling, membership, referrals, marketing
+Requires at least: 3.3
+Tested up to: 4.0
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Entice more affiliates to register by offering them a sign up bonus
+
+== Description ==
+
+> This plugin requires [AffiliateWP](http://affiliatewp.com/ "AffiliateWP") in order to function.
+
+Offering a sign up bonus is a great way to entice more affiliates to register on your site. In turn these affiliates will go on to promote your products and services, making you more sales. 
+
+**How it works**
+
+When an affiliate registers through the AfiliateWP registration form, or you manually add an affiliate from within the admin, a sign up bonus will be awarded (amount set from within the settings). This bonus is created in the form of a referral with a description of "Sign Up Bonus", which can be viewed from the Affiliates &rarr; Referrals screen in the admin. The affiliate will see this on their affiliate dashboard under "Unpaid Earnings".
+
+When "Require approval" is enabled from AffiliateWP's settings, the affiliate will not receive the sign up bonus until their application is approved. Likewise, if you manually add an affiliate from within the admin, the sign up bonus is not created until they are approved.
+
+Note: It would be a good idea to include information in your affiliate terms and conditions about a minimum payout amount, or you'll have affiliates joining and requesting a payout straight away.
+
+**What is AffiliateWP?**
+
+[AffiliateWP](http://affiliatewp.com/ "AffiliateWP") provides a complete affiliate management system for your WordPress website that seamlessly integrates with all major WordPress e-commerce and membership platforms. It aims to provide everything you need in a simple, clean, easy to use system that you will love to use.
+
+== Installation ==
+
+1. Unpack the entire contents of this plugin zip file into your `wp-content/plugins/` folder locally
+1. Upload to your site
+1. Navigate to `wp-admin/plugins.php` on your site (your WP Admin plugin page)
+1. Activate this plugin
+
+OR you can just install it with WordPress by going to Plugins >> Add New >> and type this plugin's name
+
+Finally, go to Affiliates &rarr; Settings &rarr; Integrations and enter the amount that an Affiliate should receive when they register
+
+== Screenshots ==
+
+== Upgrade Notice ==
+
+== Changelog ==
+
+= 1.0 =
+* Initial release
